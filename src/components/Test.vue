ad57c057215ce3cb775f3f85dfbc279462766c33
Ari Wijaya <ariwijaya.its@gmail.com>
2023-03-09 07:41:30 +0700

    test

---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {
+      rawHtml: '<span style="color: red">red text</span>'
+    }
+  }
+}
+</script>
+
+<template>
+  <p>Using interpolation: {{ rawHtml }}</p>
+  <p>Using directive <div v-html="rawHtml"></div></p>
+</template>
